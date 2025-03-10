8d1c952eb19bc4ac6d4cd1f70990da752696a834
João Campos <joaon.c.jv@gmail.com>
2025-03-10 20:34:30 -0300

    criando cadastro e login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,36 +1,71 @@
 import Image from "next/image";
 import comprasImg from "../assets/img/compras.png";
-import SignUp from "./signup/page";
 
 export default function Login() {
   return (
-    <main className="z-10 flex justify-between items-center w-screen h-screen m-auto">
-      <section className=" flex flex-col items-center gap-48 w-[35%]">
-          <form className="flex flex-col items-start">
-          <legend className="text-primaryColor font-bold text-4xl mb-4 text-center w-full">Login</legend>
-            <fieldset className="flex flex-col gap-4 bg-white p-8 rounded-2xl">
+    <>
+      <header className="bg-primaryColor p-8 xl:hidden">
+        <h1 className="text-white font-bold text-2xl text-left">
+          Lista de Compras
+        </h1>
+      </header>
+      <main className="flex xl:flex-row justify-between items-center w-screen h-screen m-auto">
+        <section className=" flex flex-col items-center gap-12 w-full xl:w-[35%]">
+          <form className="flex flex-col items-center p-4 w-full">
+            <legend className="text-primaryColor font-bold text-4xl mb-4 text-center w-full">
+              Login
+            </legend>
+            <fieldset className="flex flex-col gap-4 bg-white p-8 rounded-2xl w-full max-w-[500px]">
               <div className="flex flex-col">
-                <label htmlFor="email" className="text-base mb-1">e-mail</label>
-                <input type="text" id="email" className="border-2 border-gray-200 w-80 h-12 p-4" />
+                <label htmlFor="email" className="text-base mb-1">
+                  e-mail
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  className="border-2 border-gray-200 w-full h-12 p-4"
+                />
               </div>
               <div className="flex flex-col">
-                <label htmlFor="password" className="text-base mb-1">senha</label>
-                <input type="text" id="password" className="border-2 border-gray-200 w-80 h-12 p-4" />
-            </div>
-            <button type="submit" className="font-bold text-sm text-white bg-primaryColor py-2 rounded-md h-12 mt-8">Entrar</button>
-            <div className="flex gap-2 items-center justify-center">
-              <span className="text-sm text-center">Não possui conta?</span>
-                <a href="/signup" className="text-primaryColor font-bold text-sm">
+                <label htmlFor="password" className="text-base mb-1">
+                  senha
+                </label>
+                <input
+                  type="text"
+                  id="password"
+                  className="border-2 border-gray-200 w-full h-12 p-4"
+                />
+              </div>
+              <button
+                type="submit"
+                className="font-bold text-sm text-white bg-primaryColor py-2 rounded-md h-12 mt-8"
+              >
+                Entrar
+              </button>
+              <div className="flex gap-2 items-center justify-center">
+                <span className="text-sm text-center">Não possui conta?</span>
+                <a
+                  href="/signup"
+                  className="text-primaryColor font-bold text-sm"
+                >
                   Criar aqui
                 </a>
-            </div>
-            </fieldset> 
+              </div>
+            </fieldset>
           </form>
         </section>
-        <section className="bg-primaryColor w-[65%] h-full flex items-center flex-col justify-center">
-          <h1 className="text-white font-bold text-5xl text-left">Lista de Compras</h1>
-          <Image src={comprasImg} width={400} alt="compras" className="mt-8"/>
+        <section className="bg-primaryColor w-full xl:w-[65%] h-full xl:flex items-center flex-col justify-center hidden">
+          <h1 className="text-white font-bold text-5xl text-left">
+            Lista de Compras
+          </h1>
+          <Image
+            src={comprasImg}
+            width={400}
+            alt="compras"
+            className="mt-8 w-72 xl:w-96"
+          />
         </section>
-    </main>
+      </main>
+    </>
   );
 }
--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,7 +1,88 @@
-const SignUp = () => {
-    return ( 
-        <main>sign up</main>
-     );
+import Image from "next/image";
+import comprasImg from "../../assets/img/compras.png";
+
+export default function SignUp() {
+  return (
+    <>
+      <header className="bg-primaryColor p-8 xl:hidden">
+        <h1 className="text-white font-bold text-2xl text-left">
+          Lista de Compras
+        </h1>
+      </header>
+      <main className="flex xl:flex-row justify-between items-center w-screen h-screen m-auto">
+        <section className=" flex flex-col items-center gap-48 w-full xl:w-[35%]">
+          <form className="flex flex-col items-center p-4 w-full">
+            <legend className="text-primaryColor font-bold text-3xl xl:text-4xl text-center w-full">
+              Cadastro
+            </legend>
+            <fieldset className="flex flex-col gap-4 bg-white p-4 rounded-2xl w-full max-w-[500px]">
+              <div className="flex flex-col">
+                <label htmlFor="email" className="text-base mb-1">
+                  nome
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  className="border-2 border-gray-200 w-full h-12 p-4"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="email" className="text-base mb-1">
+                  e-mail
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="border-2 border-gray-200 w-full h-12 p-4"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="password" className="text-base mb-1">
+                  senha
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="border-2 border-gray-200 w-full h-12 p-4"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="password" className="text-base mb-1">
+                  Confirmar senha
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="border-2 border-gray-200 w-full h-12 p-4"
+                />
+              </div>
+              <button
+                type="submit"
+                className="font-bold text-sm text-white bg-primaryColor py-2 rounded-md h-12 mt-4"
+              >
+                Entrar
+              </button>
+              <div className="flex gap-2 items-center justify-center">
+                <span className="text-sm text-center">Já possui conta?</span>
+                <a href="/" className="text-primaryColor font-bold text-sm">
+                  Login
+                </a>
+              </div>
+            </fieldset>
+          </form>
+        </section>
+        <section className="bg-primaryColor w-full xl:w-[65%] h-full items-center flex-col justify-center hidden xl:flex">
+          <h1 className="text-white font-bold text-4xl xl:text-5xl text-left">
+            Lista de Compras
+          </h1>
+          <Image
+            src={comprasImg}
+            width={400}
+            alt="compras"
+            className="mt-4 xl:mt-8 w-52 xl:w-96"
+          />
+        </section>
+      </main>
+    </>
+  );
 }
- 
-export default SignUp;
